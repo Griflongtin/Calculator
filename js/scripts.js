d5df83752d92e74b7c06ccfba09e8772c33ea463
Griffin P Longtin and Jessica Robertson <griffin@>
2017-03-13 20:44:37 -0700

    expand on jquery rules for equilateral and isoscolese triangles.

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,27 @@ $(function() {
     var side2 = parseInt($("#side2").val());
     var side3 = parseInt($("#side3").val());
 
-    if ((side1 + side2) >= side3 || (side1 + side3) <= side2) || (side2 + side3) <= side1{
-      $("#output").text("not a triangle");
-    } else if (side1 === side2 && side2 === side3) {
+//     if ((side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1 ) {
+//       $("#output").text("not a triangle");
+//     } else if (side1 === side2 && side2 === side3 && side3 === side1) {
+//         $("#output").text("Equilateral");
+//     } else if (side1 ===  side2 && side1 !== side3) {
+//       $("#output").text("Isoscolese");
+//     } else if (side1 !== side2 && side2 !== side3 && side1 !== side3) {
+//       $("#output").text("Scalene");
+//     }
+//   });
+// });
+
+
+    if (side1 === side2 && side2 === side3 && side3 === side1) {
         $("#output").text("Equilateral");
-    } else if (side1 ===  side2 && side1 !== side3) {
+    } else if (side1 === side2 && side1 !== side3 || side1 === side3 && side1 !== side2 || side2 === side3 && side2 !== side1) {
       $("#output").text("Isoscolese");
     } else if (side1 !== side2 && side2 !== side3 && side1 !== side3) {
       $("#output").text("Scalene");
+    } else if ((side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1 ) {
+      $("#output").text("not a triangle");
     }
   });
 });
